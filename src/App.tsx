1684258aff0c1a33dfc9a8f72b1e3aa40683085a
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2026-02-08 15:02:11 -0300

    feat: add rotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Stack, Typography } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+// Componentes temporários (se você ainda não criou os reais)
+const Room = () => <h1>🃏 Sala de Poker</h1>;
+const NotFound = () => <h1>404 - Página não encontrada</h1>;
 
 function App() {
   return (
-    <Stack>
-      <Typography variant="h1">Hello Vite + React!</Typography>
-    </Stack>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:roomId" element={<Room />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
